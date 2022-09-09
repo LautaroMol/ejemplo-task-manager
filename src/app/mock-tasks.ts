a316a3d320a9c25050981f89ad74dff79e661379
Lautaro Castellanos <laumol159@gmail.com>
2022-09-09 16:18:56 -0300

    agregado mock de task

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import { Task } from "./Task"
+
+export const TASK: Task[] = [
+    {
+        id: 1,
+        Text: "terminar primer modulo angular",
+        day: "10 de septiembre 2022",
+        reminder: true
+    },
+    {
+        id: 2,
+        Text: "ejemplo de tarea 2",
+        day: "11 de septiembre 2022",
+        reminder: true
+    },
+    {
+        id: 3,
+        Text: "ejemplo de tarea 3",
+        day: "12 de septiembre 2022",
+        reminder: false
+    },
+    {
+        id: 4,
+        Text: "ejemplo de tarea 4",
+        day: "11 de septiembre 2022",
+        reminder: true
+    }
+]
