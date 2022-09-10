92e74086101b7a9ecff0a85b491b1d8c81a6e168
Lautaro Castellanos <laumol159@gmail.com>
2022-09-10 16:12:07 -0300

    lista de items

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Task } from '../../Task';
+import { TASK } from '../../mock-tasks';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task = TASK[0];
+  faTimes = faTimes;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
